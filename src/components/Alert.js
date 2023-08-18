86d0ba6dece47fa8862ccd1c21447c491fc3c1b4
Shailendra <rwtshail1@gmail.com>
2023-08-18 10:34:45 +0530

    updated files succesfully

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -6,7 +6,7 @@ export default function Alert(props) {
         return lower.charAt(0).toUpperCase()+lower.slice(1)
     }
   return (
-    <div>
+    <div style={{height:'50px'}}>
 {props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert" data-bs-dismiss="alert" >
     <strong>{capitalize(props.alert.type)}</strong>:{props.alert.msg}
 </div>}
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,12 +2,11 @@ import React, { useState } from 'react';
 
 export default function TextForm(props) {
     const [text,setText]=useState('')
-    const [preview,setPreview]=useState("Enter something in the textbox above to preview it here");
     let upperText=text.toUpperCase();
     let lowerText=text.toLowerCase();
     const handleOnChange=(event)=>{
         setText(event.target.value);
-        setPreview(event.target.value);
+        
     }
     const changeUpperCase=()=>{
         setText(upperText);
@@ -39,18 +38,18 @@ export default function TextForm(props) {
       <div className='mb-3'>
         <textarea className={`form-control `} value={text} onChange={handleOnChange} id='myBox' rows='8' style={{backgroundColor:props.mode==='light'?'#fff':'rgba(4,39,67,0.7)',color:props.mode==='light'?'#000':'#fff'}}></textarea>
       </div>
-      <button disabled={text.length===0} className="btn btn-danger" onClick={changeUpperCase}>Convert to Uppercase</button>
-      <button disabled={text.length===0} className="btn btn-danger mx-2" onClick={changeLowerCase}>Convert to Lowercase</button>
-      <button disabled={text.length===0} className="btn btn-danger mx-2" onClick={clearText}>Clear Text</button>
-      <button disabled={text.length===0} className="btn btn-danger mx-2" onClick={copyText}>Copy Text</button>
-      <button disabled={text.length===0} className="btn btn-danger mx-2" onClick={removeExtraSpace}>Remove Extra Spaces</button>
+      <button disabled={text.length===0} className="btn btn-danger mx-1 my-1" onClick={changeUpperCase}>Convert to Uppercase</button>
+      <button disabled={text.length===0} className="btn btn-danger mx-1 my-1" onClick={changeLowerCase}>Convert to Lowercase</button>
+      <button disabled={text.length===0} className="btn btn-danger mx-1 my-1" onClick={clearText}>Clear Text</button>
+      <button disabled={text.length===0} className="btn btn-danger mx-1 my-1" onClick={copyText}>Copy Text</button>
+      <button disabled={text.length===0} className="btn btn-danger mx-1 my-1" onClick={removeExtraSpace}>Remove Extra Spaces</button>
     </div>
     <div className={`container my-3 text-${(props.mode==='light')?'dark':'light'}`}>
         <h1>Your text summary</h1>
         <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
         <p>{0.008 *text.split(/\s+/).filter((element)=>{return element.length!==0}).length} Minutes read.</p>
         <h1>Preview</h1>
-        <p>{preview}</p>
+        <p>{text.length>0?text:'Nothing to Preview!'}</p>
     </div>
 
     </>
